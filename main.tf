1d33569884cc619cadd49eca791d3aff74e261dc
Houssem Medhioub <houssem.medhioub@leminnov.com>
2026-02-07 11:35:24 +0100

    Consolidates Terraform resource definitions
    Moves all resource definitions into a single main.tf file.
    
    This change streamlines the module structure and improves
    readability by centralizing the resource configurations.
    Previously, resources were spread across multiple files,
    making it harder to understand the overall infrastructure setup.

--- main.tf
+++ main.tf
@@ -1,3 +1,58 @@
+########################################
+# Security Groups
+########################################
+
+resource "outscale_security_group" "this" {
+  for_each = var.enable_security_groups ? var.security_groups : {}
+
+  description         = each.value.description
+  security_group_name = "${var.project_name}-${var.environment}-${each.key}"
+  net_id              = each.value.net_id
+
+  dynamic "tags" {
+    for_each = merge(
+      local.common_tags,
+      {
+        Name = "${var.project_name}-${var.environment}-${each.key}"
+      },
+      each.value.tags,
+    )
+    content {
+      key   = tags.key
+      value = tags.value
+    }
+  }
+}
+
+resource "outscale_security_group_rule" "this" {
+  for_each = local.sg_all_rules
+
+  flow              = each.value.flow
+  security_group_id = outscale_security_group.this[each.value.sg_key].security_group_id
+  from_port_range   = each.value.from_port_range
+  to_port_range     = each.value.to_port_range
+  ip_protocol       = each.value.ip_protocol
+  ip_range          = each.value.ip_range
+
+  security_group_account_id_to_link = each.value.security_group_account_id_to_link
+  security_group_name_to_link       = each.value.security_group_name_to_link
+}
+
+########################################
+# Keypair
+########################################
+
+resource "outscale_keypair" "this" {
+  count = var.enable_keypair ? 1 : 0
+
+  keypair_name = var.keypair_name != null ? var.keypair_name : "${var.project_name}-${var.environment}-keypair"
+  public_key   = var.keypair_public_key
+}
+
+########################################
+# VMs
+########################################
+
 resource "outscale_vm" "this" {
   for_each = local.vm_instances
 
@@ -40,4 +95,62 @@ resource "outscale_vm" "this" {
       value = tags.value
     }
   }
+}
+
+########################################
+# Volumes
+########################################
+
+resource "outscale_volume" "this" {
+  for_each = local.volume_instances
+
+  size           = each.value.size
+  volume_type    = each.value.type
+  iops           = each.value.iops
+  snapshot_id    = each.value.snapshot_id
+  subregion_name = each.value.subregion_name
+
+  dynamic "tags" {
+    for_each = each.value.tags
+    content {
+      key   = tags.key
+      value = tags.value
+    }
+  }
+}
+
+resource "outscale_volume_link" "this" {
+  for_each = local.volume_instances
+
+  volume_id   = outscale_volume.this[each.key].volume_id
+  vm_id       = outscale_vm.this[each.value.vm_key].vm_id
+  device_name = each.value.device_name
+}
+
+########################################
+# Public IPs
+########################################
+
+resource "outscale_public_ip" "this" {
+  for_each = local.vm_public_ip_instances
+
+  dynamic "tags" {
+    for_each = merge(
+      local.common_tags,
+      {
+        Name = "${each.value.name}-pip"
+      },
+    )
+    content {
+      key   = tags.key
+      value = tags.value
+    }
+  }
+}
+
+resource "outscale_public_ip_link" "this" {
+  for_each = local.vm_public_ip_instances
+
+  public_ip_id = outscale_public_ip.this[each.key].public_ip_id
+  vm_id        = outscale_vm.this[each.key].vm_id
 }
